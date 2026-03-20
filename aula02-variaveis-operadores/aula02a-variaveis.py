9e8a97a0294e21f3ad4054b1ef1e30dd1ebf7281
Jaoconturbia81 <jpconturbia@gmail.com>
2026-03-20 20:50:27 -0300

    Aula 2 variaveis e operadores

---
+++ aula02-variaveis-operadores/aula02a-variaveis.py
@@ -0,0 +1,23 @@
+print("ola mundo")
+
+print(7 + 4)
+print("7 + 4")
+print("7" + "4")  #CONCATENAÇÃO
+
+#Variaveis
+nome = "Joao" #String
+idade = 18 #int
+peso = 70.2 #Float
+
+print(nome, idade, peso)
+print(f"Oi, {nome}!!")
+
+# INPUT -- SIMULA UM FORMS NO CMD
+nome = input("Digite o seu nome:")
+idade = int(input("Digite sua idade: "))
+
+nova_idade = idade + 1
+
+print(nome, idade)
+print(nova_idade)
+
---
+++ aula02-variaveis-operadores/desafio.py
@@ -0,0 +1,18 @@
+#DESAFIO 1
+nome = input("Digite o seu nome: ")
+print("Seja Bem-Vindo")
+
+#DESAFIO 2
+dia = int(input("Digite o dia do seu nascimento"))
+mes = int(input("Digite o mes do seu nascimento"))
+ano = int(input("Digite o ano do seu nascimento"))
+
+print(f"Nasceu em {dia}/{mes}/{ano}")
+
+#DESAFIO 3
+numero1 = int(input("Digite o 1º numero"))
+numero2 = int(input("Digite o 2º numero"))
+
+total = numero1 + numero2
+
+print()
